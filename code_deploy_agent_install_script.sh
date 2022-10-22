d8190ba83a559ef874184f0f76ccce656756246a
root <root@ip-10-0-7-124.us-east-2.compute.internal>
2022-10-22 06:35:00 +0000

    Adding the script to deploy code deploy agent

---
+++ code_deploy_agent_install_script.sh
@@ -0,0 +1,23 @@
+!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install ruby-full ruby-webrick wget -y
+
+cd /tmp
+
+wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb
+
+mkdir codedeploy-agent_1.3.2-1902_ubuntu22
+
+dpkg-deb -R codedeploy-agent_1.3.2-1902_all.deb codedeploy-agent_1.3.2-1902_ubuntu22
+
+sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DEBIAN/control
+
+dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/
+
+sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb
+
+systemctl list-units --type=service | grep codedeploy
+
+sudo service codedeploy-agent status
